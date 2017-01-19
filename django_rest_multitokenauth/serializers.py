662bcfd68a71c4d1b576e04c1fa36dcfc3a0d2fd
Christian Kreuzberger <ckreuzberger@anexia-it.com>
2017-01-19 08:15:23 +0100

    forgot serializer for reset

--- django_rest_multitokenauth/serializers.py
+++ django_rest_multitokenauth/serializers.py
@@ -3,3 +3,8 @@ from rest_framework import serializers
 
 class EmailSerializer(serializers.Serializer):
     email = serializers.EmailField()
+
+
+class PasswordTokenSerializer(serializers.Serializer):
+    password = serializers.CharField(label=_("Password"), style={'input_type': 'password'})
+    token = serializers.CharField()
