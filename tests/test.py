fd9ba577add883a9f48a04c7e33134518729b78e
Christian Kreuzberger <s4ko88@gmail.com>
2017-03-12 18:36:08 +0100

    Tests: Added tests for invalid username/passwords

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,6 @@
 import json
 from django.contrib.auth.models import User
-from django.test import TestCase
-from django.urls import reverse
+from django.db.models import Q
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django_rest_multitokenauth.models import MultiToken
@@ -16,12 +15,16 @@ except:
     from django.core.urlresolvers import reverse
 
 
-class GenericTestCase(TestCase):
-    def test_generic(self):
-        self.assertEquals(True, True)
+class HelperMixin:
+    """
+    Mixin which encapsulates methods for login, logout, request reset password and reset password confirm
+    """
+    def setUpUrls(self):
+        self.login_url = reverse('multi_token_auth:auth-login')
+        self.logout_url = reverse('multi_token_auth:auth-logout')
+        self.reset_password_request_url = reverse('multi_token_auth:auth-reset-password-request')
+        self.reset_passwordconfirm_url = reverse('multi_token_auth:auth-reset-password-confirm')
 
-
-class LoginLogoutHelperMixin:
     def set_client_credentials(self, token):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
 
@@ -53,12 +56,11 @@ class LoginLogoutHelperMixin:
         )
 
 
-class LoginLogoutTestCase(APITestCase, LoginLogoutHelperMixin):
+class AuthTestCase(APITestCase, HelperMixin):
     def setUp(self):
+        self.setUpUrls()
         self.user1 = User.objects.create_user("user1", "user1@mail.com", "secret1")
         self.user2 = User.objects.create_user("user2", "user2@mail.com", "secret2")
-        self.login_url = reverse('multi_token_auth:auth-login')
-        self.logout_url = reverse('multi_token_auth:auth-logout')
 
     def login_and_obtain_token(self, username, password,  HTTP_USER_AGENT='', REMOTE_ADDR='127.0.0.1'):
         response = self.rest_do_login(username, password, HTTP_USER_AGENT, REMOTE_ADDR)
@@ -90,3 +92,91 @@ class LoginLogoutTestCase(APITestCase, LoginLogoutHelperMixin):
 
         # there should now again be zero tokens in the database
         self.assertEqual(MultiToken.objects.all().count(), 0)
+
+    def test_login_multiple_times(self):
+        # there should be zero tokens
+        self.assertEqual(MultiToken.objects.all().count(), 0)
+
+        # login first time
+        token1 = self.login_and_obtain_token('user1', 'secret1')
+        self.assertEqual(MultiToken.objects.all().count(), 1)
+        # verify the token is for user 1
+        self.assertEqual(
+            MultiToken.objects.filter(key=token1).first().user.username,
+            'user1'
+        )
+
+        # login second time
+        token2 = self.login_and_obtain_token('user1', 'secret1')
+        self.assertEqual(MultiToken.objects.all().count(), 2)
+        # verify the token is for user 1
+        self.assertEqual(
+            MultiToken.objects.filter(key=token2).first().user.username,
+            'user1'
+        )
+        # verify that token1 is not equal to token2
+        self.assertNotEqual(token1, token2)
+
+        # login third time
+        token3 = self.login_and_obtain_token('user1', 'secret1')
+        self.assertEqual(MultiToken.objects.all().count(), 3)
+        # verify the token is for user 1
+        self.assertEqual(
+            MultiToken.objects.filter(key=token3).first().user.username,
+            'user1'
+        )
+        # verify that token is not equal to token2 and token3
+        self.assertNotEqual(token1, token3)
+        self.assertNotEqual(token2, token3)
+
+        # Now logout with token2 and verify that token1 and token3 are still in database
+        response = self.rest_do_logout(token2)
+        self.assertEqual(response.status_code, 200)
+        # should be two tokens left
+        self.assertEqual(MultiToken.objects.all().count(), 2)
+        # verify that these two are token1 and token3
+        self.assertEqual(
+            MultiToken.objects.filter(Q(key=token1) | Q(key=token3)).count(),
+            2
+        )
+
+    def test_login_with_invalid_credentials(self):
+        # correct username, but wrong password
+        response = self.rest_do_login("user1", "wrongpassword")
+        content = json.loads(response.content.decode())
+        self.assertEqual(response.status_code, 400)
+        self.assertTrue('non_field_errors' in content)
+
+        # there should be zero tokens
+        self.assertEqual(MultiToken.objects.all().count(), 0)
+
+        # wrong username, but correct password
+        response = self.rest_do_login("userOne", "secret1")
+        content = json.loads(response.content.decode())
+        self.assertEqual(response.status_code, 400)
+        self.assertTrue('non_field_errors' in content)
+
+        # there should be zero tokens
+        self.assertEqual(MultiToken.objects.all().count(), 0)
+
+        # wrong username, wrong password
+        response = self.rest_do_login("userOne", "wrongpassword")
+        content = json.loads(response.content.decode())
+        self.assertEqual(response.status_code, 400)
+        self.assertTrue('non_field_errors' in content)
+
+        # there should be zero tokens
+        self.assertEqual(MultiToken.objects.all().count(), 0)
+
+
+    def test_login_multiple_users(self):
+        pass
+
+    def test_reset_password(self):
+        pass
+
+    def test_reset_password_multiple_users(self):
+        pass
+
+    def test_reset_password_multiple_users(self):
+        pass
