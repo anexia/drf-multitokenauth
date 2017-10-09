436c21483e5e392731312248c665ce3a0e3b6163
Christian Kreuzberger <ckreuzberger@anexia-it.com>
2017-10-09 09:51:37 +0200

    added app_name to urls.py for django 2.0 compatibility

--- django_rest_multitokenauth/urls.py
+++ django_rest_multitokenauth/urls.py
@@ -3,6 +3,8 @@
 from django.conf.urls import url, include
 from django_rest_multitokenauth.views import login_and_obtain_auth_token, logout_and_delete_auth_token
 
+app_name = 'django_rest_multitokenauth'
+
 urlpatterns = [
     url(r'^login', login_and_obtain_auth_token, name="auth-login"),  # normal login with session
     url(r'^logout', logout_and_delete_auth_token, name="auth-logout")
