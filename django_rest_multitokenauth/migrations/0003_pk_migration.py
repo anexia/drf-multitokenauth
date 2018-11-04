d008b6a728a57362feea3e01b8d3dd25a1462f67
Harald Nezbeda <hn@nezhar.com>
2018-11-04 19:33:25 +0100

    Fix django migrations

--- django_rest_multitokenauth/migrations/0003_pk_migration.py
+++ django_rest_multitokenauth/migrations/0003_pk_migration.py
@@ -15,13 +15,8 @@ def populate_auto_incrementing_pk_field(apps, schema_editor):
         o.save()
 
 
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ('django_rest_multitokenauth', '0002_rename_ip_address_20160426',),
-    ]
-
-    operations = [
+def get_migrations_for_django_before_21():
+    return [
         migrations.AddField(
             model_name='multitoken',
             name='id',
@@ -45,3 +40,49 @@ class Migration(migrations.Migration):
             field=models.CharField(db_index=True, max_length=64, unique=True, verbose_name='Key'),
         ),
     ]
+
+
+def get_migrations_for_django_21_and_newer():
+    return [
+        # remove primary key information from 'key' field
+        migrations.AlterField(
+            model_name='multitoken',
+            name='key',
+            field=models.CharField(db_index=True, primary_key=False, max_length=64, unique=True, verbose_name='Key'),
+        ),
+        # add a new id field
+        migrations.AddField(
+            model_name='multitoken',
+            name='id',
+            field=models.AutoField(primary_key=True, serialize=False),
+            preserve_default=False,
+        ),
+        migrations.RunPython(
+            populate_auto_incrementing_pk_field,
+            migrations.RunPython.noop
+        ),
+    ]
+
+
+def get_migrations_based_on_django_version():
+    """
+    Returns the proper migrations based on the current Django Version
+    Unfortunatley, Django 2.1 introduced a breaking change with switching PK from one model to another, see
+    https://code.djangoproject.com/ticket/29790
+    :return:
+    """
+    django_version = django.VERSION
+
+    if django_version[0] >= 2 and django_version[1] >= 1:
+        return get_migrations_for_django_21_and_newer()
+
+    return get_migrations_for_django_before_21()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('django_rest_multitokenauth', '0002_rename_ip_address_20160426',),
+    ]
+
+    operations = get_migrations_based_on_django_version()
